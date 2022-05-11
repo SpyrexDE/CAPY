bdc75af30523d2ee2188f0d5dd0fa6cdbd7b1cfc
SpyrexDE <malikfk100@gmail.com>
2022-05-11 15:07:32 +0200

    Polish player movement

--- src/characters/player/Player.gd
+++ src/characters/player/Player.gd
@@ -22,8 +22,10 @@ var x_input : float = 0.0
 var y_input : float = 0.0
 
 # Logic
-var air_jumps := 1
+var max_air_jumps := 1
+var air_jumps := 0
 var can_jump := true
+var jump_pressed := false
 var should_jump := false
 
 # Node References
@@ -38,14 +40,18 @@ func _ready() -> void:
 
 #Movement
 func _physics_process(delta) -> void:
-	handleInput()
 	handleCollsision()
+	handleInput()
+	handleMovement(delta)
 	handleAnimation()
-
+	
 
 func handleInput() -> void:
 	x_input = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	y_input = Input.get_action_strength("jump")
+
+	jump_pressed = Input.is_action_just_pressed("jump")
+	should_jump = Input.is_action_pressed("jump")
 
 	if Input.is_action_just_pressed("attack"):
 		#attack anim
@@ -56,53 +62,62 @@ func handleCollsision() -> void:
 	
 	if is_on_floor():
 		can_jump = true
+		air_jumps = max_air_jumps
+
+func handleMovement(delta: float) -> void:
+	# X-Acceleration
+	motion.x += x_input * ACCELERATION * delta * TARGET_FPS
+	motion.x = clamp(motion.x, -MAX_SPEED, MAX_SPEED)
+	# Gravity
+	motion.y += GRAVITY * delta * TARGET_FPS
+	
+	# X-Friction
+	if is_on_floor():
+		# floor
+		if !is_moving() && is_on_floor():
+			motion.x = lerp(motion.x, 0, FRICTION * delta)
+	else:
+		# air
+		if !is_moving() && !is_on_floor():
+			motion.x = lerp(motion.x, 0, AIR_RESISTANCE * delta)
+
+	# Jumping
+	if jump_pressed:
+		if is_on_floor() && can_jump:
+			motion.y = -JUMP_FORCE
+			can_jump = false
+		
+		# Air jumping
+		if !is_on_floor() and air_jumps > 0:
+				motion.y = -JUMP_FORCE
+				can_jump = false
+				air_jumps -= 1
+	
+	# Make jump shorter when no longer pressed
+	if !should_jump && !is_on_floor():
+		motion.y += lerp(0, GRAVITY * delta * TARGET_FPS, 0.8)
+
+	# Apply motion and rotation
+	motion = move_and_slide(motion, Vector2.UP, false, 4, PI/4, false)
+	setRot(Utils.completeInt(x_input))
 
 func handleAnimation():
 	if is_moving():
 		if is_on_floor():
 			pass
 			# run
-		motion.x += x_input * ACCELERATION * delta * TARGET_FPS
-		motion.x = clamp(motion.x, -MAX_SPEED, MAX_SPEED)
-		
-		setRot(Utils.completeInt(x_input))
-		
-	elif is_on_floor():
-		# idle anim
-		pass
-	
-	motion.y += GRAVITY * delta * TARGET_FPS
+	else:
+		if is_on_floor():
+			# idle anim
+			pass
 	
 	if !is_on_floor() && !is_on_wall():
 		# jump anim
-		
-		if !Input.is_action_pressed("jump"):
-			motion.y /= 2
-	
-	if is_on_floor() && can_jump:
-		if x_input == 0:
-			motion.x = lerp(motion.x, 0, FRICTION * delta)
-			
-		if y_input != 0 || should_jump == true:
-			motion.y = -JUMP_FORCE
-			can_jump = false
-	else:
-		if y_input != 0 and motion.y < -JUMP_FORCE/2:
-			motion.y = -JUMP_FORCE/2
-		if x_input == 0:
-			motion.x = lerp(motion.x, 0, AIR_RESISTANCE * delta)
-		
-		if !is_on_floor() and air_jumps > 0:
-			if x_input == 0:
-				motion.x = lerp(motion.x, 0, FRICTION * delta)
-			
-			if y_input != 0 || should_jump == true:
-				motion.y = -JUMP_FORCE
-				can_jump = false
-	
-	motion = move_and_slide(motion, Vector2.UP, false, 4, PI/4, false)
+		pass
 
 func setRot(rot):
+	if rot == 0:
+		return
 	$Tween.interpolate_property($Rotator, "scale", $Rotator.scale, Vector2(rot,1), 0.05, Tween.TRANS_CUBIC, Tween.EASE_OUT)
 	$Tween.start()
 	
