c7526ebd9ee3b1116977aa4990a28523898a7418
SpyrexDE <malikfk100@gmail.com>
2022-05-15 14:48:14 +0200

    Update to Godot 4
    Alpha 8

--- src/characters/player/Player.gd
+++ src/characters/player/Player.gd
@@ -1,41 +1,39 @@
-extends KinematicBody2D
+extends CharacterBody2D
 
 #############
 # Variables #
 #############
 
 # Constants
-export (int, 1, 120) var TARGET_FPS := 60
-export (int, 10, 50) var ACCELERATION := 50
-export (int, 50, 800) var MAX_SPEED := 400
-export (int, 0, 30) var FRICTION := 15
-export (int, 0, 5) var AIR_RESISTANCE := 1
-export (int, 0, 100) var GRAVITY := 20
-export (int, 0, 1000) var JUMP_FORCE := 500
-export (int, 0, 200) var PUSH := 100
-export (float, 0, 1) var FORGIVE_TIME := 0.2
-export (int, 0, 10) var MAX_AIR_JUMPS := 2
+@export var TARGET_FPS := 60
+@export var ACCELERATION := 50
+@export var MAX_SPEED := 400
+@export var FRICTION := 15
+@export var AIR_RESISTANCE := 1
+@export var GRAVITY := 20
+@export var JUMP_FORCE := 500
+@export var PUSH := 100
+@export var FORGIVE_TIME := 0.2
+@export var MAX_AIR_JUMPS := 2
 
 # Physics
-var motion := Vector2.ZERO
-onready var aimed_scale = self.scale 
+@onready var aimed_scale = self.scale 
 
 # Input
 var x_input : float = 0.0
 var y_input : float = 0.0
 
 # Logic
-onready var air_jumps := MAX_AIR_JUMPS
+@onready var air_jumps := MAX_AIR_JUMPS
 var can_jump := true
 var jump_pressed := false
 var should_jump := false
 var jumptimer_active := false
 
 # Child node references
-onready var cJumpTimer = $JumpTimer
-onready var cAnimationTree = $AnimationTree
-onready var cTween = $Tween
-onready var cCamera2D = $Camera2D
+@onready var cJumpTimer = $JumpTimer
+@onready var cAnimationTree = $AnimationTree
+@onready var cCamera2D = $Camera2D
 
 #############
 # Functions #
@@ -60,7 +58,7 @@ func handleInput() -> void:
 	should_jump = Input.is_action_pressed("jump")
 
 	if Input.is_action_just_pressed("attack"):
-		#attack anim
+		#attack anims
 		pass
 
 func handleCollsision() -> void:
@@ -75,39 +73,39 @@ func handleCollsision() -> void:
 
 func handleMovement(delta: float) -> void:
 	# X-Acceleration
-	motion.x += x_input * ACCELERATION * delta * TARGET_FPS
-	motion.x = clamp(motion.x, -MAX_SPEED, MAX_SPEED)
+	velocity.x += x_input * ACCELERATION * delta * TARGET_FPS
+	velocity.x = clamp(velocity.x, -MAX_SPEED, MAX_SPEED)
 	# Gravity
-	motion.y += GRAVITY * delta * TARGET_FPS
+	velocity.y += GRAVITY * delta * TARGET_FPS
 	
 	# X-Friction
 	if is_on_floor():
 		# floor
 		if !is_moving() && is_on_floor():
-			motion.x = lerp(motion.x, 0, FRICTION * delta)
+			velocity.x = lerp(velocity.x, 0, FRICTION * delta)
 	else:
 		# air
 		if !is_moving() && !is_on_floor():
-			motion.x = lerp(motion.x, 0, AIR_RESISTANCE * delta)
+			velocity.x = lerp(velocity.x, 0, AIR_RESISTANCE * delta)
 	
 	# Jumping
 	if jump_pressed:
 		if can_jump:
-			motion.y = -JUMP_FORCE
+			velocity.y = -JUMP_FORCE
 			can_jump = false
 		
 		# Air jumping
 		if !is_on_floor() and air_jumps > 0:
-				motion.y = -JUMP_FORCE
+				velocity.y = -JUMP_FORCE
 				can_jump = false
 				air_jumps -= 1
 	
 	# Make jump shorter when no longer pressed
 	if !should_jump && !is_on_floor():
-		motion.y += GRAVITY * delta * TARGET_FPS
+		velocity.y += GRAVITY * delta * TARGET_FPS
 
-	# Apply motion and rotation
-	motion = move_and_slide(motion, Vector2.UP, false, 4, PI/4, false)
+	# Apply velocity and rotation
+	move_and_slide()
 	
 	if x_input != 0:
 		flipH(true if x_input < 0 else false)
@@ -131,12 +129,12 @@ func flipH(flip:bool):
 			child.scale.x = (-1 if flip else 1) * abs(child.scale.x)
 
 func checkPush() -> void:
-	for index in get_slide_count():
+	for index in get_slide_collision_count():
 		var collision = get_slide_collision(index)
-		if !collision.collider:
+		if !collision.get_collider():
 			return
-		if collision.collider.is_in_group("bodies"):
-			collision.collider.apply_central_impulse(-collision.normal * PUSH)
+		if collision.get_collider().is_in_group("bodies"):
+			collision.get_collider().apply_central_impulse(-collision.normal * PUSH)
 
 
 ###########
