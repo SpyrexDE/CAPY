6a1f69df25b4d0dfeef6625d4b3d806f73dfdd71
SpyrexDE <malikfk100@gmail.com>
2022-08-02 16:34:23 +0200

    Rework startmenu buttons

--- src/public/autoload/utils.gd
+++ src/public/autoload/utils.gd
@@ -1,1 +1,11 @@
 extends Node
+
+static func calc_outline_scale(size: Vector2, scale: float, absoulte := false) -> Vector2:
+	var width = size.x
+	var height = size.y
+	
+	if width > height:
+		scale = scale * (width / height)
+	
+	var factor = Vector2(1 + height * scale / width, 1 + scale)
+	return factor
