f4f302da49c2f9b2c5c657aa608d749cb387a793
SpyrexDE <malikfk100@gmail.com>
2022-09-18 19:52:51 +0200

    Add base for settings, add TODO addon

---
+++ src/global/autoload/settings_manager.gd
@@ -0,0 +1,24 @@
+extends Node
+
+enum GRAPHICS {
+	LOW,
+	MEDIUM,
+	HIGH
+}
+
+var OPTIONS := {
+	"General": {
+		"General Setting": false
+	},
+	"Sound": {
+		"Sound Setting example": 0.2
+	},
+	"Visuals": {
+		"Visual level example": GRAPHICS.LOW
+	},
+	"Controls": {
+		"forward": "i dont know"
+	},
+	
+}
+
--- src/ui/menus/settings/settings.gd
+++ src/ui/menus/settings/settings.gd
@@ -1,14 +1,15 @@
 extends Control
 
-const PAGES = ["res://src/ui/menus/settings/pages/general.tscn"]
+const PAGE = preload("res://src/ui/menus/settings/page.tscn")
 
 @export
 var tab_container : TabContainer
 
 func _ready() -> void:
-	for page in PAGES:
-		var p : Control = load(page).instantiate()
-		tab_container.add_child(p)
+	# TODO: For category in settingsManager instantiate new page with settings controls
+	var page_name = ""
+	var p : Control = PAGE.new(page_name)
+	tab_container.add_child(p)
 
 func get_focus() -> void:
 	tab_container.get_children()[0].get_focus()
