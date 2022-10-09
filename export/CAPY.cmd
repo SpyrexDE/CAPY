078089461dc51eb0169aea28848edd0eae59febf
SpyrexDE <malikfk100@gmail.com>
2022-10-09 14:15:23 +0200

    Remove export and build directory

--- export/CAPY.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-title "CAPY"
-"%~dp0CAPY.exe" "%*"
-pause > nul
--- export/CAPY.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo -ne '\033c\033]0;CAPY\a'
-base_path="$(dirname "$(realpath "$0")")"
-"$base_path/CAPY.x86_64" "$@"
